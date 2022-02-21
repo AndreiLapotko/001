df22cdb08fc59e011d2c776d3d52a714af02ffca
Andrei <shudyx@gmail.com>
2022-02-22 00:53:11 +1000

    тестируем работу с git

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+//"use strict";
+const result = confirm('Are you here?');
+result ? console.log('Да') : console.log('Нет');
+
+const answer = prompt('Вам есть 18?', 'да');
+console.log(answer);
+
+const user = 'Andrei';
+alert(`Привет, ${user}`);
